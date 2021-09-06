08f8affe5035d57ed3b0e6998ab5672ec730989d
lsy <lsy971021@gmail.com>
2021-09-06 17:22:29 +0800

    add search

--- es-demo/src/main/java/com/lsy/Application.java
+++ es-demo/src/main/java/com/lsy/Application.java
@@ -1,11 +1,50 @@
 package com.lsy;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        //SpringApplication.run(Application.class,args);
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(RestClient.builder(
+                new HttpHost("localhost", 9200, "http")));
+        SearchRequest searchRequest = new SearchRequest("index03");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        searchSourceBuilder.query(QueryBuilders.rangeQuery("age").gte(20));
+
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SearchHit[] hits = search.getHits().getHits();
+        for (int i = 0; i < hits.length; i++) {
+            String id = hits[i].getId();
+            Object age = hits[i].getSourceAsMap().get("age");
+            System.out.println("id=="+id+"----- age="+age);
+        }
+        //System.out.println(search);
+        //System.out.println(search.getHits());
+        //System.out.println(search.getHits().getHits());
+        try {
+            restHighLevelClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
