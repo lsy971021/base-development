f4aaabe1d5d38397fe2dd01a535197f863da2374
lsy <lsy971021@gmail.com>
2021-09-16 18:56:28 +0800

    create rocketmq

---
+++ rocketMQ-demo/src/main/java/com/lsy/content/MQInfo.java
@@ -0,0 +1,8 @@
+package com.lsy.content;
+
+public class MQInfo {
+    public static String TOPIC = "topicv1";
+    public static String PRODUCERGROUP = "progroupv1";
+    public static String COMSUMERGROUP = "congroupv1";
+    public static String TAG = "tagv1";
+}
---
+++ rocketMQ-demo/src/main/java/com/lsy/listener/MessageListenerV1.java
@@ -0,0 +1,18 @@
+package com.lsy.listener;
+
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+//@Component
+public class MessageListenerV1 implements MessageListenerConcurrently {
+    @Override
+    public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+        System.out.println(list.toString());
+        System.out.println(consumeConcurrentlyContext.getMessageQueue());
+        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+    }
+}
