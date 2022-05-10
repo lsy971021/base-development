0e04c61c4085a5747b876c23cbe2306acbfa1e4f
lsy <lsy971021@gmail.com>
2022-05-10 23:25:24 +0800

    mybatis核心配置文件，支持多环境数据源environment配置,全局变量properties配置,别名typeAliases配置

--- mybatis/src/main/java/com/lsy/test/Test.java
+++ mybatis/src/main/java/com/lsy/test/Test.java
@@ -17,7 +17,9 @@ public class Test {
         //创建工厂对象
         SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
         //一个factory代表一个数据源，从创建开始一直运行
-        SqlSessionFactory factory = sqlSessionFactoryBuilder.build(stream);
+        //参数可指定environment（sqlMapperConfig.xml中配置的id）
+        //第三个参数可指定properties属性，此优先级最高
+        SqlSessionFactory factory = sqlSessionFactoryBuilder.build(stream,"development");
         SqlSession session = factory.openSession();
         List<Object> find = session.selectList("find");
         //添加log4j可查看控制台输出的sql语句动态拼接
