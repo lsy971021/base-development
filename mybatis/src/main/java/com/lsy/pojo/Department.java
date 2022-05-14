4d0508b215de77e17159bc73281a2aff794aeb4f
lsy <lsy971021@gmail.com>
2022-05-14 22:10:53 +0800

    mybatis:缓存 init

--- mybatis/src/main/java/com/lsy/pojo/Department.java
+++ mybatis/src/main/java/com/lsy/pojo/Department.java
@@ -4,9 +4,14 @@ import lombok.Data;
 
 @Data
 public class Department {
+
     private int id;
+
     private String name;
+
     private int age;
+
     private int gender;
+
     private String team;
 }
