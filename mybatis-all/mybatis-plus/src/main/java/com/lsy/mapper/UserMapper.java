2089f7528818352328c580ad8ccc6e51b86f31fb
liusiyuan <lsy971021@gmail.com>
2022-07-20 14:08:32 +0800

    add:mybatis-plus多数据源dynamic

--- mybatis-all/mybatis-plus/src/main/java/com/lsy/mapper/UserMapper.java
+++ mybatis-all/mybatis-plus/src/main/java/com/lsy/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package com.lsy.mapper;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lsy.model.User;
 
 import java.util.List;
 
+@DS("master")
 public interface UserMapper extends BaseMapper<User>{
     List<User> find();
 }
