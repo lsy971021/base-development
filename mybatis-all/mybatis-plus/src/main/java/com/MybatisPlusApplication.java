325a1ce35574ec0fb021ad8ca5f5c77cd8ff8e14
lsy <lsy971021@gmail.com>
2022-10-11 00:53:19 +0800

    启动类排除spring Boot 2.0以上默认连接池hikari：DataSourceAutoConfiguration.class

--- mybatis-all/mybatis-plus/src/main/java/com/MybatisPlusApplication.java
+++ mybatis-all/mybatis-plus/src/main/java/com/MybatisPlusApplication.java
@@ -3,8 +3,12 @@ package com;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * 若pom中若添加了druid-spring-boot-starter依赖，则需要排除 DruidDataSourceAutoConfigure.class，否则排除pring Boot 2.0以上默认连接池hikari：DataSourceAutoConfiguration.class
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("com.lsy.mapper")
 public class MybatisPlusApplication {
     public static void main(String[] args) {
